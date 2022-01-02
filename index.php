ee5c5fe551de42de7370d8d7cddf934ac4bcdc7f
lydiaeck <lydia.v.eck@gmail.com>
2022-01-02 11:41:49 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    
+    <title>Lawyer Page</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Oswald:wght@200;300;400;500;600;700&display=swap"
+      rel="stylesheet"
+    />
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.js"
+      integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+      crossorigin="anonymous"
+    ></script>
+
+    <link href="style.css" rel="stylesheet" />
+
+    <style>
+                                          
+    </style>
+  </head>
+  <body>
+
+    <div
+      id="carouselExampleDark"
+      class="carousel carousel-dark slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-indicators">
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active" data-bs-interval="10000">
+          <img
+            src="justice.jpg"
+            class="d-block w-100"
+            alt="..."
+          />
+          <div class="carousel-caption d-none d-md-block glow">
+            <h5>L.V.E. Law Firm</h5>
+            <p>Quality legal services at an affordable cost</p>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+
+    <div class="container-md" id="carry">
+      <div class="row">
+        <div class="col-12">
+          <h1>Get the Judgment You Need</h1>
+        </div>
+      </div>
+
+      <nav id="link" style="margin-bottom: 0.6em">
+        <div class="item" style="border-right: 1px solid #999">
+          <a href="home">Home</a>
+        </div>
+        <div class="item" style="border-right: 1px solid #999">
+          <a href="about">About</a>
+        </div>
+        <div class="item" style="border-right: 1px solid #999">
+          <a href="services">Services</a>
+        </div>
+        <div class="item"><a href="content">Contact</a></div>
+      </nav>
+
+      <div class="row">
+        <div class="col-4 padding bio2">
+          <img src="lincoln.jpg" alt="Abraham Lincoln" id="lincoln" />
+        </div>
+        <div class="col-8 padding bio">
+          <p>
+            <strong>Abraham Lincoln</strong>
+            <em>(/ˈlɪŋkən/; February 12, 1809 – April 15, 1865)</em> was an
+            American lawyer and statesman who served as the 16th president of
+            the United States from 1861 until his assassination in 1865. Lincoln
+            led the nation through the American Civil War and succeeded in
+            preserving the Union, abolishing slavery, bolstering the federal
+            government, and modernizing the U.S. economy.
+          </p>
+
+          <p>
+            Lincoln was born into poverty in a log cabin and was raised on the
+            frontier primarily in Indiana. He was self-educated and became a
+            lawyer, Whig Party leader, Illinois state legislator, and U.S.
+            Congressman from Illinois. In 1849, he returned to his law practice
+            but became vexed by the opening of additional lands to slavery as a
+            result of the Kansas–Nebraska Act. He reentered politics in 1854,
+            becoming a leader in the new Republican Party, and he reached a
+            national audience in the 1858 debates against Stephen Douglas.
+            Lincoln ran for President in 1860, sweeping the North in victory.
+            Pro-slavery elements in the South equated his success with the
+            North's rejection of their right to practice slavery, and southern
+            states began seceding from the Union. To secure its independence,
+            the new Confederate States fired on Fort Sumter, a U.S. fort in the
+            South, and Lincoln called up forces to suppress the rebellion and
+            restore the Union.
+          </p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-6 lines padding email">
+          &copy;
+          <a
+            href="http://www.lydiaeck.com"
+            style="text-decoration: none; color: rgb(71, 16, 16)"
+            >LVE Designs</a
+          >
+          2022
+        </div>
+        <div
+          class="col-6 linetop padding"
+          style="text-align: center; font-style: italic"
+        >
+          <span>myemail@gmail.com</span>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
